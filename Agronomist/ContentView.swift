6d10bcfb3f39ed6b1271ef58710d3f0d5df68544
Ryan Thally <rthally94@gmail.com>
2020-06-01 09:44:17 -0400

    Initial Commit

---
+++ Agronomist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Agronomist
+//
+//  Created by Ryan Thally on 6/1/20.
+//  Copyright © 2020 Ryan Thally. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
